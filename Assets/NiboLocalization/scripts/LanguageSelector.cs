6f29d2aafcf15844aef7c5fae498c2473b10d761
Johnatan Botero <johnatna@jbotero.com>
2017-10-08 13:03:43 -0500

    CreateProject
    Setup Project on GIT.

---
+++ Assets/NiboLocalization/scripts/LanguageSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageSelector : MonoBehaviour {
+
+    public Text inLanguageCode;
+    public Text lblActiveLanguage;
+
+     void Awake()
+    {
+       lblActiveLanguage.text = ("Active Language: " + LocalizationText.GetLanguage());
+    }
+
+    public void LanguageSelectorAction(){
+        LocalizationText.SetLanguage(inLanguageCode.text);
+        print(inLanguageCode.text); 
+        lblActiveLanguage.text = ("Active Language: " +LocalizationText.GetLanguage());
+    }
+}
---
+++ Assets/NiboLocalization/scripts/LocalizationCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizationCode : MonoBehaviour {
+
+    public Text T1, T2, T3;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		T1.text = LocalizationText.GetText("lbl-settings");
+		T2.text = LocalizationText.GetText("lbl-acept");
+		T3.text = LocalizationText.GetText("lbl-cancel");
+	}
+}
